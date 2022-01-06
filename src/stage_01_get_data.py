0a770b8cacdfb1c76abb8c8b422004fdcf5639ca
victen <vikramnimmakuri@gmail.com>
2022-01-06 06:31:45 -0800

    stage 01 updated finally with doc string

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -3,18 +3,18 @@ import os
 import shutil
 from tqdm import tqdm
 import logging
-from src.utils.common import read_yaml, create_directories,save_json,copy_files
+from src.utils.common import read_yaml, create_directories, save_json, copy_files
 import random
 
 
-STAGE = "STAGE_NAME" ## <<< change stage name 
+STAGE = "STAGE_NAME"  ## <<< change stage name
 
 logging.basicConfig(
-    filename=os.path.join("logs", 'running_logs.log'), 
-    level=logging.INFO, 
+    filename=os.path.join("logs", "running_logs.log"),
+    level=logging.INFO,
     format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
-    filemode="a"
-    )
+    filemode="a",
+)
 
 
 def get_data(config_path, params_path):
@@ -22,13 +22,17 @@ def get_data(config_path, params_path):
     source_download_dirs = config["source_download_dirs"]
     local_data_dirs = config["local_data_dirs"]
 
-    for source_download_dir,local_data_dir in tqdm(zip(source_download_dirs,local_data_dirs),total=2,desc="list of the folders",colour="red"):
+    for source_download_dir, local_data_dir in tqdm(
+        zip(source_download_dirs, local_data_dirs),
+        total=2,
+        desc="list of the folders",
+        colour="red",
+    ):
         create_directories([local_data_dir])
-        copy_files(source_download_dir,local_data_dir)
-    
+        copy_files(source_download_dir, local_data_dir)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
     args.add_argument("--params", "-p", default="params.yaml")
@@ -36,9 +40,9 @@ if __name__ == '__main__':
 
     try:
         logging.info("\n********************")
-        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        logging.info(f">>>>> stage 01 started <<<<<")
         get_data(config_path=parsed_args.config, params_path=parsed_args.params)
-        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+        logging.info(f">>>>> stage 01 completed!<<<<<\n")
     except Exception as e:
         logging.exception(e)
         raise e
--- src/utils/common.py
+++ src/utils/common.py
@@ -6,26 +6,29 @@ import pandas as pd
 import json
 import shutil
 
+
 def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
     logging.info(f"yaml file: {path_to_yaml} loaded successfully")
     return content
 
+
 def create_directories(path_to_directories: list) -> None:
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
         logging.info(f"created directory at: {path}")
+
 
 def copy_files(source_download_dir: str, local_data_dir: str) -> None:
     list_of_files = os.listdir(source_download_dir)
     N = len(list_of_files)
 
     for file in list_of_files:
-        src = os.path.join(source_download_dir,file)
-        dest = os.path.join(local_data_dir,file)
+        src = os.path.join(source_download_dir, file)
+        dest = os.path.join(local_data_dir, file)
 
-        shutil.copy(src,dest)
+        shutil.copy(src, dest)
 
     logging.info("copy of files succeded")
 
