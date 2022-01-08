c0a31094fb117d0d31cddcf633475196d4cb29e8
victen <vikramnimmakuri@gmail.com>
2022-01-08 05:35:58 -0800

    updated callbacks

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -26,7 +26,7 @@ def create_and_save_checkpointing_callbacks(callbacks_dir: str,checkpoint_dir: s
     checkpoint_file = os.path.join(checkpoint_dir,"ckpt_model.h5")
     checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_file,save_best_only=True)
 
-    ckpt_callback_filepath = os.path.join(callbacks_dir,"tensorboard_cb.cb")
+    ckpt_callback_filepath = os.path.join(callbacks_dir,"checkpoint_cb.cb")
     joblib.dump(checkpoint_callback,ckpt_callback_filepath)
 
     logging.info(f"checkpoint callback is saved at {ckpt_callback_filepath} as binary file")
